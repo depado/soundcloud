fcb916252bb538da90a872ce446d78fc3e0c793d
Paul Lhussiez <paul.lhussiez@adevinta.com>
2019-12-09 10:53:13 +0100

    Adding tests

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.13
 
 require (
 	github.com/go-resty/resty/v2 v2.1.0
-	golang.org/x/net v0.0.0-20190923162816-aa69164e4478 // indirect
+	github.com/stretchr/testify v1.4.0
 )
--- service_test.go
+++ service_test.go
@@ -4,9 +4,10 @@ import (
 	"testing"
 
 	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_service_Limit(t *testing.T) {
+func Test_service_Get(t *testing.T) {
 	type fields struct {
 		client      *resty.Client
 		queryParams map[string]string
@@ -14,54 +15,49 @@ func Test_service_Limit(t *testing.T) {
 		pathParams  map[string]string
 	}
 	type args struct {
-		limit int
+		out interface{}
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
 	}{
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := &service{
+			s := service{
 				client:      tt.fields.client,
 				queryParams: tt.fields.queryParams,
 				path:        tt.fields.path,
 				pathParams:  tt.fields.pathParams,
 			}
-			s.Limit(tt.args.limit)
+			if err := s.Get(tt.args.out); (err != nil) != tt.wantErr {
+				t.Errorf("service.Get() error = %v, wantErr %v", err, tt.wantErr)
+			}
 		})
 	}
 }
 
+func Test_service_Limit(t *testing.T) {
+	s := service{}
+	assert.NotContains(t, s.queryParams, "limit", "query params should be empty by default")
+	s.Limit(1)
+	assert.Contains(t, s.queryParams, "limit", "limit should be contained")
+	assert.Equal(t, s.queryParams["limit"], "1", "limit should be equal to value")
+	s.Limit(2)
+	assert.Contains(t, s.queryParams, "limit", "limit should be contained")
+	assert.Equal(t, s.queryParams["limit"], "2", "limit should be equal to value")
+}
+
 func Test_service_Offset(t *testing.T) {
-	type fields struct {
-		client      *resty.Client
-		queryParams map[string]string
-		path        string
-		pathParams  map[string]string
-	}
-	type args struct {
-		offset int
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &service{
-				client:      tt.fields.client,
-				queryParams: tt.fields.queryParams,
-				path:        tt.fields.path,
-				pathParams:  tt.fields.pathParams,
-			}
-			s.Offset(tt.args.offset)
-		})
-	}
+	s := service{}
+	assert.NotContains(t, s.queryParams, "offset", "query params should be empty by default")
+	s.Offset(1)
+	assert.Contains(t, s.queryParams, "offset", "offset should be contained")
+	assert.Equal(t, s.queryParams["offset"], "1", "offset should be equal to value")
+	s.Offset(2)
+	assert.Contains(t, s.queryParams, "offset", "offset should be contained")
+	assert.Equal(t, s.queryParams["offset"], "2", "offset should be equal to value")
 }
